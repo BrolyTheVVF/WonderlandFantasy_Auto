e6031baef6a95ba235b7b89e203d43bd642d620e
BrolyTheVVF <BrolyTheVVF@gmail.com>
2025-05-18 13:43:20 +0200

    Update UI_Auto.js
    First simple version of attacking a player

--- UI_Auto.js
+++ UI_Auto.js
@@ -19,6 +19,9 @@ game.auto.HTML = false;
 game.auto.current = {
 	"map": false,
 	"npcList": {},
+	"active": false,
+	"state": "idle",
+	"tickDelay": 0,
 };
 game.auto.slots = {
 	"skills": [false, false, false, false, false, false],
@@ -78,7 +81,7 @@ game.auto.refreshUI = function(){
 	if(!game.auto.__isBuild){
 		game.auto.buildInterface();
 	}
-	if(!game.auto.visible || !game.player || !game.scenes.list.main.scene.visible){
+	if(!game.auto.visible || !game.player || !game.scene.main.scene.visible){
 		$("#AUTO_UI_MAIN").hide();
 		return;
 	}
@@ -123,7 +126,7 @@ game.auto.refreshUI_Lang = function(){
 	// $("#AUTO_UI_MAIN #character-window-stat-LUCK-label").html(LC_TEXT("UI.stat.LUCK.short"));
 };
 
-game.auto.refreshUI_Gears = function(){
+game.auto.refreshUI_Npcs = function(){
 	let oChar = game.player;
 	if(oChar === false){ return; }
 };
@@ -152,6 +155,221 @@ game.auto.pullWindow = function(){
 
 
 
+game.auto.onTick = function(){
+	if(!game.player){
+		requestAnimationFrame(game.auto.onTick);
+		return;
+	}
+	
+	if(Date.now() < game.auto.current.tickDelay){
+		requestAnimationFrame(game.auto.onTick);
+		return;
+	}
+	
+	if(game.auto.current.map !== game.player.map){
+		game.auto.current.map = game.player.map;
+		game.auto.current.npcList = {};
+		
+	}
+	game.auto.npcList.check();
+	if(game.auto.current.active === true){
+		game.auto.pickupItems();
+		game.auto.pickupSouls();
+		game.auto.onTickEvent[game.auto.current.state]();
+	}else{
+		game.auto.current.state = "idle";
+	}
+	
+	requestAnimationFrame(game.auto.onTick);
+};
+
+game.auto.onTickEvent = {};
+game.auto.onTickEvent.idle = function(){
+	if(game.player.lockOn){
+		game.auto.setState("combat");
+		return;
+	}
+	//Search for next target
+	let oClosest = game.utilities.getClosestEntity();
+	if(oClosest){
+		game.setLockON(oClosest.uid, true, "auto");
+		game.auto.setState("combat");
+	}
+};
+game.auto.onTickEvent.combat = function(){
+	if(!game.player.lockOn || game.player.lockOn.isDead){
+		game.auto.setState("idle");
+		return;
+	}
+	if(!game.auto.Combat_isInRange()){
+		game.auto.setState("reaching");
+		return;
+	}
+	let sSkillBase = game.player.classe + "_base";
+	let oSkillBase = game.player.skills[sSkillBase];
+	if(oSkillbase.isReady(game.player)){
+		game.player.askCastSkillOn(sSkillBase, game.player.lockOn);
+	}
+	
+	for(let i = 0; i < game.auto.slots.length; i++){
+		let SkillID = game.auto.slots[i];
+		let oSkill = game.player.skills[SkillID];
+		let oProto = oSkill.proto;
+		if(oProto.isPassive){
+			continue;
+		}
+		if(!oProto.targetEnnemy){
+			continue;
+		}
+		
+		if(oSkill.isReady(game.player)){
+			game.player.askCastSkillOn(sSkillBase, game.player.lockOn);
+			return;
+		}
+	};
+};
+game.auto.onTickEvent.reaching = function(){
+	if(!game.player.lockOn || game.player.lockOn.isDead){
+		game.auto.setState("idle");
+		return;
+	}
+	if(game.auto.Combat_isInRange()){
+		game.auto.setState("combat");
+		return;
+	}
+	if(!game.player.isWalking){
+		game.setTarget(game.player.lockOn.x, game.player.lockOn.y);
+	}
+	
+};
+game.auto.setState = function(sState){
+	game.auto.current.state = sState;
+	console.log("Auto -> new state", sState);
+	if(sState === "idle"){
+		if(game.player.isWalking){
+			game.setTarget(game.player.x, game.player.y);
+		}
+		game.player.inAutoAttack = false;
+	}else if(sState === "combat" || sState === "reaching"){
+		if(!game.player.lockOn || game.player.lockOn.isDead){
+			game.auto.setState("idle");
+			return;
+		}
+		if(sState === "combat"){
+			if(game.player.isWalking){
+				game.setTarget(game.player.x, game.player.y);
+			}
+			game.player.inAutoAttack = true;
+		}else{
+			game.player.inAutoAttack = false;
+		}
+	}
+	game.auto.current.tickDelay = Date.now() + 500;
+}
+
+game.auto.Combat_isInRange = function(){
+	let sSkillBase = game.player.classe + "_base";
+	if(!game.player.skills[sSkillBase].inRange(game.player, game.player.lockOn)){
+		return false;
+	}
+	
+	for(let i = 0; i < game.auto.slots.length; i++){
+		let SkillID = game.auto.slots[i];
+		let oSkill = game.player.skills[SkillID];
+		let oProto = oSkill.proto;
+		if(oProto.isPassive){
+			continue;
+		}
+		if(!oProto.targetEnnemy){
+			continue;
+		}
+		if(!oSkill.inRange(game.player, game.player.lockOn)){
+			return false;
+		}
+	};
+	
+	return true;
+};
+game.auto.pickupItems = function(){
+	let l = [];
+	for(let k in game.groundItems){
+		let oDrop = game.groundItems[k];
+		if(!oDrop){
+			continue;
+		}
+		if(oDrop.owners && oDrop.owners !== false && oDrop.owners.indexOf(game.player.uid) < 0){
+			continue;
+		}
+		l.push(k);
+	}
+	game._emit("pickupMultipleItem", [l]);
+};
+game.auto.pickupSouls = function(){
+	if(game.SOUL_GATHERING.isActive() && game.player.specialsCount.SOUL_GATHERING_LAST !== false){
+		let nColor = game.player.specialsCount.SOUL_GATHERING_LAST;
+		if(!nColor){
+			nColor = 2;
+		}
+		let oGatherList = [];
+		for(let k in game.SOUL_GATHERING.list){
+			if(game.SOUL_GATHERING.list[k].color == nColor){
+				oGatherList.push(k);
+			}
+		}
+		game.SOUL_GATHERING.pickupSoulList(oGatherList);
+	}
+};
+
+
+
+game.auto.npcList = {};
+game.auto.npcList.lastCheck = 0;
+game.auto.npcList.check = function(){
+	if(!game.player){return;}
+	if(game.auto.npcList.lastCheck + 2500 > Date.now()){return;}
+	let bUpdate = false;
+	
+	game.auto.npcList.lastCheck = Date.now();
+	
+	for(let k in game.entities){
+		let o = game.entities[k];
+		if(o.isPC){
+			continue;
+		}
+		if(!game.player.canDamage(o)){
+			continue;
+		}
+		let MID = o.mid;
+		if(!game.auto.current.npcList.hasOwnProperty(MID)){
+			game.auto.current.npcList[MID] = new game.auto.npcList.proto(o);
+			bUpdate = true;
+		}
+	}
+	if(bUpdate){
+		game.auto.refreshUI_Npcs();
+	}
+};
+
+game.auto.npcList.proto = class{
+	constructor(o){
+		//Maybe at some point handle the idle stance ???
+		if(!(o instanceof NPC)){
+			throw new Error("Entity list object must be an instance of the NPC class");
+		}
+		this.skinTexture = false;
+		if(o.skins.base.frames.hasOwnProperty("idle0")){
+			this.skinTexture = o.skins.base.frames.idle0[0].sprite._texture.baseTexture.textureCacheIds[0];
+		}else{
+			
+		}
+		this.mid = o.mid;
+		this.selected = true;
+		this.level = o.level;
+	}
+}
+
+
+
 $(document).ready(() => {
 	game.UI.list.push(game.auto);
 	locale["UI.windows.auto.title"] = {"en": "Auto-pilot", "fr": "Pilote automatique"};
@@ -159,6 +377,7 @@ $(document).ready(() => {
 	locale["UI.windows.auto.tab.settings"] = {"en": "Settings", "fr": "Paramètres"};
 	locale["UI.windows.auto.tab.monsters"] = {"en": "Monsters", "fr": "Monstres"};
 	
+	game.auto.onTick();
 });
 
 
