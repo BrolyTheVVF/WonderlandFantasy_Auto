1e672b66fa9f6aecceff1f3d2a5ced1ece880483
BrolyTheVVF <BrolyTheVVF@gmail.com>
2025-05-18 09:08:06 +0200

    Create Tempermonkey script.js
    Temporary, add this to the main desc later

---
+++ Tempermonkey script.js
@@ -0,0 +1,9 @@
+Go to utilities
+
+Import from URL
+
+Paste :     https://raw.githubusercontent.com/BrolyTheVVF/WonderlandFantasy_Auto/refs/heads/main/UI_Auto.js
+
+clic install
+
+Should keep itself updated from the github repo every day
